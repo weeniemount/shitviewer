000ff3449b91a0522ad49283b050be183a69a676
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-06-17 12:34:34 +0200

    add name to title

--- src/index.js
+++ src/index.js
@@ -17,20 +17,20 @@ function createWindow() {
 	win.loadFile(path.join(__dirname, 'viewer/readmeviewer.html'));
 
 	const fileSubmenu = [
-		{ label: 'June 11 2025 - 7:50', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june11-2025-7-50.md')) },
-		{ label: 'June 13 2025 - 19:43', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june13-2025-19-43.md')) },
-		{ label: 'June 13 2025 - 20:35', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june13-2025-20-35.md')) },
-		{ label: 'June 13 2025 - 21:10', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june13-2025-21-10.md')) },
-		{ label: 'June 14 2025 - 10:33', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june14-2025-10-33.md')) },
-		{ label: 'June 14 2025 - 5:44', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june14-2025-5-44.md')) },
-		{ label: 'June 15 2025 - 15:27', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-27.md')) },
-		{ label: 'June 15 2025 - 15:28', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-28.md')) },
-		{ label: 'June 15 2025 - 15:30', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-30.md')) },
-		{ label: 'June 15 2025 - 15:41', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-41.md')) },
-		{ label: 'June 15 2025 - 18:02', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-02.md')) },
-		{ label: 'June 15 2025 - 18:05', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-05.md')) },
-		{ label: 'June 15 2025 - 18:05 (02)', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-05-02.md')) },
-		{ label: 'June 15 2025 - 18:05 (03)', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-05-03.md')) }
+		{ label: 'June 11 2025 - 7:50', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june11-2025-7-50.md'), 'June 11 2025 - 7:50') },
+		{ label: 'June 13 2025 - 19:43', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june13-2025-19-43.md',), 'June 13 2025 - 19:43') },
+		{ label: 'June 13 2025 - 20:35', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june13-2025-20-35.md',), 'June 13 2025 - 20:35') },
+		{ label: 'June 13 2025 - 21:10', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june13-2025-21-10.md',), 'June 13 2025 - 21:10') },
+		{ label: 'June 14 2025 - 10:33', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june14-2025-10-33.md',), 'June 14 2025 - 10:33') },
+		{ label: 'June 14 2025 - 5:44', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june14-2025-5-44.md'),  'June 14 2025 - 5:44') },
+		{ label: 'June 15 2025 - 15:27', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-27.md'), 'June 15 2025 - 15:27') },
+		{ label: 'June 15 2025 - 15:28', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-28.md'), 'June 15 2025 - 15:28') },
+		{ label: 'June 15 2025 - 15:30', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-30.md'), 'June 15 2025 - 15:30') },
+		{ label: 'June 15 2025 - 15:41', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-15-41.md'), 'June 15 2025 - 15:41') },
+		{ label: 'June 15 2025 - 18:02', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-02.md'), 'June 15 2025 - 18:02') },
+		{ label: 'June 15 2025 - 18:05', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-05.md'), 'June 15 2025 - 18:05') },
+		{ label: 'June 15 2025 - 18:05 (02)', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-05-02.md'), 'June 15 2025 - 18:05 (02)') },
+		{ label: 'June 15 2025 - 18:05 (03)', click: () => win.webContents.send('load-md', path.join(__dirname, 'readmes/june15-2025-18-05-03.md'), 'June 15 2025 - 18:05 (03)') }
 	];
 
 	const menu = Menu.buildFromTemplate([
